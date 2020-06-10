4c70f3a2188f25a2d14ad490b6670b28b91b4517
Nikita Koba <nikitakoba93@gmail.com>
2020-06-10 17:18:43 -0400

    first commit

---
+++ Cleaner Code/goodpep8.py
@@ -0,0 +1,26 @@
+def foo_bar(arg1, arg2, arg3, arg4):
+    return arg1, arg2, arg3, arg4
+
+def bar(*args):
+    return 2 + 2
+
+class Treehouse:
+    def one(self):
+        return 1
+    
+    def two(self):
+        return 2
+
+alpha, beta, charlie, delta = foo_bar(
+    "a long string", 
+    "a longer string", 
+    "yet another long string", 
+    "and other crazy string")
+
+one = 1
+three = 3
+fourteen = 14  # make fourteen equal to 12
+
+print(alpha)
+print(fourteen)
+print(Treehouse().two())
